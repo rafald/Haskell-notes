5cd9ad75c43efa711bf215077181c0b5b8b12310
Rafal Rafalski <gotchas.u@gmail.com>
2017-05-18 00:50:16 +0200

    refine types involeved in mvsk: mean, variance, skewness, and kurtosis fro moments

--- one_pass_stats.hs
+++ one_pass_stats.hs
@@ -1,3 +1,13 @@
+#!/usr/bin/env stack
+-- stack runghc --resolver lts-8.0 --install-ghc
+
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE PartialTypeSignatures  #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
+moments :: forall t.
+            Floating t =>
+            (t, t, t, t, t) -> t -> (t, t, t, t, t)
 moments (n, m1, m2, m3, m4) x = (n', m1', m2', m3', m4')
         where
             n' = n + 1
@@ -11,7 +21,10 @@ moments (n, m1, m2, m3, m4) x = (n', m1', m2', m3', m4')
             m2' = m2 + t
 
 -- mean, variance, skewness, and kurtosis fro moments
-mvsk (n, m1, m2, m3, m4) = (m1, m2/(n-1.0), (sqrt n)*m3/m2**1.5, n*m4/m2**2 - 3.0)
+mvsk :: forall t t1.
+          Floating t1 =>
+          (t1, t, t1, t1, t1) -> (t, t1, t1, t1)
+mvsk (n, m1, m2, m3, m4) = (m1, m2/(n-1.0), sqrt n*m3/m2**1.5, n*m4/m2**2 - 3.0)
 
 {- The foldl applies moments first to its initial value, the 5-tuple of zeros.
 Then it iterates over the data, taking data points one at a time and visiting each point only once,
@@ -20,18 +33,25 @@ Another way to say this is that after processing each data point,
 moments returns the 5-tuple that it would have returned
 if that data only consisted of the values up to that point
 -}
-online_stats = mvsk . foldl moments (0,0,0,0,0)
+onlineStats :: [Double] -> (Double, Double, Double, Double)
+onlineStats = mvsk . foldl moments (0,0,0,0,0)
 
+-- TODO return Maybe or Either
+-- TODO it seems that t type is inferred from calling function !
+-- safeRead :: forall t. Read t => String -> t
+safeRead :: Read t => String -> t
 safeRead inStr = case reads inStr of
-  [] -> error $ "Parse failed for: " ++ inStr
+  []         -> error $ "Parse failed for: " ++ inStr
   (parse1:_) -> fst parse1
 
+main :: IO ()
 main = do
+  print $ Just 3
   rs <- sequence [getLine, getLine, getLine, getLine]
   print rs
-  print $ online_stats $ map safeRead rs
+  -- DOES NOT COMPILE print $ map safeRead rs
+  print $ onlineStats $ map safeRead rs -- only first number from each sublist is used 
 
-  print $ online_stats [2, 30, 51, 72]
-  print $ Just 3
+  print $ onlineStats [2, 30, 51, 72]
 -- prints (38.75, 894.25,-0.1685, -1.2912)
 --        (38.75,894.25,-0.16847151077905,-1.29117407893914)
